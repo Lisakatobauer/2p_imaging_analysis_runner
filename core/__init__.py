26a8b21fa0fe3b362f699a89be138ae0a17f8ce2
Lisakatobauer <34125695+Lisakatobauer@users.noreply.github.com>
2025-06-06 11:56:18 +0200

    Update __init__.py

---
+++ core/__init__.py
@@ -0,0 +1,8 @@
+# twop_imaging_pipeline/__init__.py
+
+from .suite2p_processor import Suite2pProcessor
+from .suite2p_traces import Suite2pTraces
+from .suite2p_loader import Suite2pLoader
+from .suite2p_visualiser import Suite2pVisualiser
+
+__all__ = ["Suite2pProcessor", "Suite2pTraces", "Suite2pLoader", "Suite2pVisualiser"]
